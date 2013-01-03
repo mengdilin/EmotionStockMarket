d61723b00d7fc94f6dcd0e037197fe3e7dc2c475
Victoria <victoria.stempel@gmail.com>
2013-01-03 12:22:31 -0500

    added Z's auth code

---
+++ app.py
@@ -0,0 +1,49 @@
+#THIS IS Z'S CODE
+
+from flask import Flask
+from flask import session,render_template,url_for,redirect,request
+import googleauth
+
+app = Flask(__name__)
+app.secret_key="secret key" # Since we'll be using sessions
+
+@app.route("/")
+def index():
+    if not session.has_key('user'):
+        return redirect(url_for('login'))
+    return render_template("index.html",d=session['user'])
+
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect(url_for('login'))
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    else:
+        url=googleauth.build_redirect_url()
+        return redirect(url)
+
+@app.route("/googleoauth2callback")
+def googleoauth2callback():
+    # this code comes from the google login page
+    code=request.args.get('code','')
+
+    # if we didn't get a code, go back to login
+    if code=='':
+        return redirect(url_for('login'))
+
+    # We have a code so we have to convert it to an access token
+    access_token = googleauth.code_to_access_token(code)
+
+    # and convert it to userinfo
+    userinfo=googleauth.access_token_to_info(access_token)
+    session['user']=userinfo
+    return redirect(url_for('index'));
+
+if __name__=="__main__":
+    app.debug=True
+    app.run(host='ml7.stuycs.org',port=4500)
