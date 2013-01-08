ea251b4fd3d8fdf66f0eefe6bf29574013b00c54
gyang01 <georgiana.yang@yahoo.com>
2013-01-08 12:30:07 -0500

    writing database still

--- utils.py
+++ utils.py
@@ -30,24 +30,43 @@ def add_stock(name,stock,n,c):
     emotion={"emotion":stock, "amount":c}
     if hasMoney(name,n,c):
         if hasStock(name,stock):
-            updateStock(name,stock,n)
+            updateStock(name,stock,n,True)
         else:
             stocks=getStocks(name)
             nstocks=stocks.insert(emotion)
-            players.update({'name':str(name)},{"$push":{'stocks':nstocks} } )
-        changeMoney(name,n,c)
+            users.update({'name':str(name)},{"$push":{'stocks':nstocks} } )
+        changeMoney(name,n,c,True)
 
-def changeMoney(name,n,c):
+def changeMoney(name,n,c,buy):
     """
-    deducts money from account
+    Changes the money in a player's account after buy/sell stock
     name:used to identify player
-    n:number of stocks bought
+    n:number of stocks
     c:price per stock
+    'buy' is a boolean: True if buying stock; False otherwise
+    DONE
     """
     db=Connection['EmotionStock']
     user=db.players
     cost=int(float(n))*int(float(c))
-    myMoney=
+    myMoney=getMoney(name)
+    if buy:
+        myMoney=myMoney-cost
+    else:
+        myMoney=myMoney+cost
+    user.update({'name':str(name)},{"$push": {"money":myMoney}})
+
+def updateStock(name,stock,n,buy):
+    """
+    User already has stocks of a given emotion
+    Updates the amount
+    name: player
+    stock: name of stock
+    n: amount of stock
+    buy: boolean; True if buying; False if selling
+    """
+    myStocks=getStocks(name)
+    
 
 def hasMoney(name,n,c):
     """
@@ -56,22 +75,17 @@ def hasMoney(name,n,c):
     """
     db=Connection['EmotionStock']
     user=db.players
-    player=players.find({'name':str(name)})
+    player=user.find({'name':str(name)})
     myMoney=float(player["money"])
     cost=int(n)*int(c)
     total=myMoney-cost
     return total>=0
     
-
-#def updateStock
-
 def hasStock(name,stock):
     """
     returns boolean: True if has 'stock'; False if it doesn't
     DONE
     """
-    db=Connection['EmotionStock']
-    user=db.players
     stocks=getStocks(name)
     return stocks.find({'name':str(stock)}).count()
 
@@ -97,3 +111,13 @@ def getStocks(name):
     user=db.players
     player=user.find({'name':str(name)})
     return player['stocks']
+
+def getMoney(name):
+    """
+    returns player's money
+    DONE
+    """
+    db=Connection['EmotionStock']
+    user=db.players
+    player=user.find({'name':str(name)})
+    return float(player["money"])
