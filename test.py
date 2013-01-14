121e27c132908dc55b7204cd09c3bcd7de2c17c1
gyang01 <georgiana.yang@yahoo.com>
2013-01-14 01:36:00 -0500

    tested some things; need to correct them

--- test.py
+++ test.py
@@ -9,8 +9,15 @@ def auth():
 
 auth()
 db=Connection['test_g_final']
-users=db.players
-user.insert({"name":"me","money":"100","soul":"100","stocks":{}})
-res=users.find()
+user=db.players
+#db.players.drop()
+#user.insert({"name":"me","money":"100","soul":"100","stocks":{}})
+res=user.find()
 for line in res:
     print line
+l=user.find({"name":"me"})
+print l
+for item in l:
+    print item["money"] #<- this is what i need to do!!!
+#FIX FIX FIX!!!
+#find() only prints a cursor...
